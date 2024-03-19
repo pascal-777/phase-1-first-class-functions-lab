3ba22bc885e52f74d029614a721895f654f02a98
pascal <pascal.nicintije@student.moringaschool.com>
2024-03-19 15:51:21 +0300

    adds index.html

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers= function (drivers) {
+    return drivers.slice(2,)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(fare) {
+        return fare * num
+    }
+}
+const fareDoubler = createFareMultiplier => createFareMultiplier * 2;
+
+const fareTripler = createFareMultiplier => createFareMultiplier * 3;
+
+function selectDifferentDrivers(drivers, selectingDrivers ) {
+    return selectingDrivers(drivers);
+}
